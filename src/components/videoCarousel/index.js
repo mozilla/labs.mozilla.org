6c8976f104b864418f29c0a1b74d540107bbc4e7
Vlad Shumov <shumov.vlad@gmail.com>
2018-11-09 16:23:37 +0300

    update

---
+++ src/components/videoCarousel/index.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react'
+import withStyles from 'isomorphic-style-loader/lib/withStyles'
+
+import styles from './index.less'
+
+export default withStyles(styles)(
+    class extends Component {
+        static defaultProps = {
+            onRef() {}
+        };
+
+        componentDidMount() {
+            this.props.onRef(this);
+        }
+
+        render() {
+            console.log('123', this.props);
+
+            const {
+                title,
+                description,
+                videoCarousel
+            } = this.props;
+
+            return <div className={styles.content}>
+                <div className={styles.inner}>
+                    <div className={styles.text}>
+                        <h1>{title}</h1>
+                        <p>{description}</p>
+                    </div>
+
+                    {videoCarousel && videoCarousel.length && <div className="slider">
+                        {videoCarousel
+
+                            // .filter(Boolean) // TODO: reVideo
+                            .map(({id, title, video: {url = ''} = {}}) =>
+                                <video
+                                    key={id}
+                                    src={url}
+                                />
+                            )}
+                    </div>}
+                </div>
+            </div>
+        }
+    }
+)
