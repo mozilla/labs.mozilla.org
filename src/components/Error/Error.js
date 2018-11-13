6767b544865422f2db566f71c74c264a22edc55a
Vlad Shumov <shumov.vlad@gmail.com>
2018-11-13 11:46:52 +0300

    update home page styles, add err, add slider

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -15,8 +15,18 @@ export default withStyles(styles)(class extends Component {
         //}
 
         return <div className={styles.container}>
-            <h1>Error.</h1>
-            <a href="/" className="button" data-type="button">back</a>
+            <div className={styles.text}>
+                <h1>Whoops!</h1>
+
+                <ul>
+                    <li>If you typed in the address, check your spelling. Could just be a typo.</li>
+                    <li>If you’ve found an issue with one of our websites, we’d appreciate it if you could report the problem in Bugzilla, our bug tracker. One of our developers will take a look at it as soon as possible.</li>
+                    <li>If you followed a link, it’s probably broken.</li>
+                    <li>If you’re not sure what you’re looking for, start at mozilla.org.</li>
+                </ul>
+
+                <a href="/" className="button" data-type="button">Back</a>
+            </div>
         </div>
     }
 })
--- src/components/videoCarousel/index.js
+++ src/components/videoCarousel/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Swiper from 'swiper/dist/js/swiper.min'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 
 import styles from './index.less'
@@ -11,6 +12,43 @@ export default withStyles(styles)(
 
         componentDidMount() {
             this.props.onRef(this);
+        }
+
+        initSwiper = async el => {
+            // if(this.props.videoCarousel && !this.props.videoCaraousel.length) {}
+            if(!el || this.swiper) {
+                return
+            }
+
+            const self = this;
+
+            self.swiper = new Swiper(el, {
+                direction: 'horizontal',
+                wrapperClass: styles.wrapper,
+                slideClass: styles.slide,
+                slideActiveClass: styles.active,
+                slideNextClass: styles.next,
+                slidePrevClass: styles.prev,
+                slideToClickedSlide: false,
+                pagination: {
+                    el: `.${styles['slider-pagination']}`,
+                    clickable: true,
+                    bulletClass: styles.bullet,
+                    bulletActiveClass: styles.active
+                },
+                navigation: {
+                    prevEl: `.${styles['slider-btn-prev']}`,
+                    nextEl: `.${styles['slider-btn-next']}`,
+                    disabledClass: styles.disabled
+                }
+            })
+        };
+
+        componentWillUnmount() {
+            if(this.swiper && this.swiper.destroy instanceof Function) {
+                // TODO: test
+                this.swiper.destroy(true, false)
+            }
         }
 
         render() {
@@ -27,17 +65,47 @@ export default withStyles(styles)(
                         <p>{description}</p>
                     </div>
 
-                    {videoCarousel && videoCarousel.length && <div className="slider">
-                        {videoCarousel
-
-                            // .filter(Boolean) // TODO: reVideo
-                            .map(({id, title, video: {url = ''} = {}}) =>
-                                <video
-                                    key={id}
-                                    src={url}
-                                />
-                            )}
+                    {videoCarousel && videoCarousel.length && <div
+                        ref={this.initSwiper}
+                        className={styles.container}
+                    >
+                        <div className={styles.wrapper}>
+                            {videoCarousel
+                                .filter(Boolean) // TODO: reVideo
+                                .map((
+                                    {
+                                        id,
+                                        title,
+                                        video: {
+                                            url = ''
+                                        } = {}
+                                    },
+                                    index) =>
+                                    <video
+                                        key={id + index}
+                                        className={styles.slide}
+                                        preload={'true'}
+                                        controls
+                                        src={url}
+                                    />
+                                )}
+                        </div>
                     </div>}
+
+                    {videoCarousel && videoCarousel.length > 1 && [
+                        <div className={styles['slider-pagination']}/>,
+
+                        <div className={styles['slider-btn-prev']}>
+                            <svg width="33" height="61" viewBox="0 0 33 61" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M31.698 57.154a2 2 0 1 1-2.828 2.829L.586 31.698a2 2 0 0 1 0-2.828L28.87.586a2 2 0 1 1 2.828 2.828l-26.87 26.87 26.87 26.87z" fill="#CCC" />
+                            </svg>
+                        </div>,
+                        <div className={styles['slider-btn-next']}>
+                            <svg width="33" height="61" viewBox="0 0 33 61" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M31.698 57.154a2 2 0 1 1-2.828 2.829L.586 31.698a2 2 0 0 1 0-2.828L28.87.586a2 2 0 1 1 2.828 2.828l-26.87 26.87 26.87 26.87z" fill="#CCC" />
+                            </svg>
+                        </div>
+                    ]}
                 </div>
             </div>
         }
