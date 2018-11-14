558b2e3c58587fe51fa7f68dd56dd900da39d843
Vlad Shumov <shumov.vlad@gmail.com>
2018-11-14 11:48:12 +0300

    mobile menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,44 +1,138 @@
 import React, {Component} from 'react'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
+import {connect} from 'react-redux'
+
+import {
+    desktopMenuVisibilityHandler,
+    mobileMenuVisibilityHandler
+} from '../../utils/store'
 
 import styles from './index.scss'
 
 export default withStyles(styles)(
-    class extends Component {
-        static defaultProps = {
-            onRef() {}
-        };
+    connect(
+        ({
+            desktopMenuVisibility,
+            mobileMenuVisibility,
+            headerInverted,
+            headerVisibility
+        }) => ({
+            desktopMenuVisibility,
+            mobileMenuVisibility,
+            headerInverted,
+            headerVisibility
+        })
+    )(
+        class extends Component {
+            static defaultProps = {
+                onRef() {}
+            };
 
-        componentDidMount() {
-            this.props.onRef(this);
-        }
+            componentDidMount() {
+                this.props.onRef(this);
+            }
 
-        render() {
-            const {
-                theme,
-                logo: {
-                    url,
-                    title,
-                } = {},
-                items
-            } = this.props;
+            bodyOverflow = bool => {
+                if(process.env.BROWSER) {
+                    const body = document.body;
 
-            return <header className={
-                `${styles.content} ${theme ? styles.dark : ''}`
-            }>
-                <div className={styles.inner}>
-                    <a href="/" className={styles.logo}>
-                        <img src={url} alt={title}/>
-                    </a>
+                    if(body) {
+                        body.style.overflow = bool ? 'visible' : 'hidden';
+                    }
+                }
+            };
 
-                    <div className={styles.text}>
-                        {items && items.length && items
-                            .map(([Component, props], index) =>
-                                <Component key={props.id + index} {...props}/>
-                            )}
+            get viewportWidth() {
+                return process.env.BROWSER ?
+                    window.innerWidth ||
+                    document.documentElement.clientWidth ||
+                    document.body.clientWidth :
+                    0
+            }
+
+            switchVisibility = () => {
+                const {
+                    dispatch,
+                    desktopMenuVisibility,
+                    mobileMenuVisibility
+                } = this.props;
+
+                dispatch(
+                    this.viewportWidth < 769 ?
+                        mobileMenuVisibilityHandler(!mobileMenuVisibility) :
+                        desktopMenuVisibilityHandler(!desktopMenuVisibility)
+                );
+
+                this.bodyOverflow(mobileMenuVisibility);
+            };
+
+            closeMobile = () => {
+                const {
+                    dispatch
+                } = this.props;
+
+                if(this.viewportWidth < 769) {
+                    dispatch(
+                        mobileMenuVisibilityHandler(false)
+                    );
+
+                    this.bodyOverflow(1)
+                }
+            };
+
+            render() {
+                const {
+                    location: {pathname},
+                    theme,
+                    logo: {
+                        url,
+                        title,
+                    } = {},
+                    items,
+
+                    desktopMenuVisibility,
+                    mobileMenuVisibility
+                } = this.props;
+
+                return <header className={[
+                    styles.content,
+                    theme ? styles.dark : '',
+                    desktopMenuVisibility && styles.desktopOpened,
+                    mobileMenuVisibility && styles.mobileOpened
+                ].filter(Boolean).join(' ')}>
+                    <div className={styles.inner}>
+                        <a
+                            href="/"
+                            className={styles.logo}
+                            onClick={this.closeMobile}
+                        >
+                            <img src={url} alt={title}/>
+                        </a>
+
+                        <ul>
+                            {items && items.length && items
+                                .map(([Component, props], index) =>
+                                    <li
+                                        onClick={this.closeMobile}
+                                        key={props.id + index}
+                                        className={props.url === pathname ? styles.active : ''}
+                                    >
+                                        <Component {...props}/>
+                                    </li>
+                                )}
+                        </ul>
+
+                        <div className={styles.overlay} onClick={this.switchVisibility}/>
+
+                        <button
+                            className={styles.openMenu}
+                            onClick={this.switchVisibility}
+                        >
+                            <span/><span/><span/>
+                        </button>
                     </div>
-                </div>
-            </header>
+                </header>
+            }
         }
-    }
+    )
 )
--- src/utils/store.js
+++ src/utils/store.js
@@ -37,38 +37,10 @@ const headerVisibility = (state = false, {type, state: isHidden}) =>
     type === HEADER_VISIBILITY ? !!isHidden : state;
 
 
-export const VIDEO_MODAL = 'VIDEO_MODAL';
-export const videoModalHandler = (isVisible, url) => ({
-    type: VIDEO_MODAL,
-    state: isVisible,
-    url: isVisible ? url : ''
-})
-const videoModal = (state = false, {type, state: isVisible, url}) => {
-    if(type === VIDEO_MODAL) {
-        return {
-            state: type === VIDEO_MODAL ? !!isVisible : state,
-            url
-        }
-    }
-    return null
-}
-
-
-export const FORM_MODAL_VISIBILITY = 'FORM_MODAL_VISIBILITY';
-export const formModalVisibilityHandler = isVisible => ({
-    type: 'FORM_MODAL_VISIBILITY',
-    state: isVisible
-})
-const formModalVisibility = (state = false, {type, state: isVisible}) =>
-    type === FORM_MODAL_VISIBILITY ? !!isVisible : state;
-
-
 const reducer = combineReducers({
     desktopMenuVisibility,
     mobileMenuVisibility,
-    formModalVisibility,
     headerVisibility,
-    videoModal
 
     //isScrolling,
 });
