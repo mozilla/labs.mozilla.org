becac458ec1249fb8662a4f0abe7b8622a0158bf
Vlad Shumov <shumov.vlad@gmail.com>
2018-11-16 13:21:37 +0300

    option for button: scroll next section

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -13,6 +13,24 @@ export default withStyles(styles)(
             this.props.onRef(this);
         }
 
+        scrollNext = e => {
+            if(process.env.BROWSER) {
+                const section = e.target.closest('section');
+
+                if(section !== null) {
+                    const height = section.offsetHeight;
+                    const top = section.offsetTop;
+
+                    if(Number(height) && top === 0 || Number(top)) {
+                        window.scrollTo({
+                            top: top + height,
+                            behavior: 'smooth'
+                        });
+                    }
+                }
+            }
+        };
+
         render() {
             const {
                 title,
@@ -22,6 +40,7 @@ export default withStyles(styles)(
                 theme,
                 inverted,
                 type,
+                scrollToNextBlock = false,
                 image: {
                     url: imageUrl = '',
                     details: {
@@ -32,9 +51,11 @@ export default withStyles(styles)(
                 } = {}
             } = this.props;
 
-            if(type) {
+            if(type || !url) {
                 return <button
                     type={type}
+                    onClick={scrollToNextBlock ? this.scrollNext : () => {}}
+                    style={scrollToNextBlock ? {cursor: 'pointer'} : {}}
                     className={
                         `${styles.button}
                         ${buttonClass ? styles[buttonClass] : ''}
@@ -45,25 +66,25 @@ export default withStyles(styles)(
                 >
                     <span>{title}</span>
                 </button>
-            } else {
-                return <a
-                    href={url}
-                    className={
-                        `${styles.button}
-                        ${buttonClass ? styles[buttonClass] : ''}
-                        ${size ? styles[size] : ''}
-                        ${inverted ? styles.inverted : ''}
-                        ${theme ? styles[theme] : ''}`
-                    }
-                >
-                    {imageUrl ?
-                        <img
-                            src={`${imageUrl}?scale=fit&w=${Math.ceil(width/2).toFixed()}`}
-                            srcSet={`${imageUrl} 2x`}
-                            alt={title}/> :
-                        <span>{title}</span>}
-                </a>
             }
+
+            return <a
+                href={url}
+                className={
+                    `${styles.button}
+                    ${buttonClass ? styles[buttonClass] : ''}
+                    ${size ? styles[size] : ''}
+                    ${inverted ? styles.inverted : ''}
+                    ${theme ? styles[theme] : ''}`
+                }
+            >
+                {imageUrl ?
+                    <img
+                        src={`${imageUrl}?scale=fit&w=${Math.ceil(width/2).toFixed()}`}
+                        srcSet={`${imageUrl} 2x`}
+                        alt={title}/> :
+                    <span>{title}</span>}
+            </a>
         }
     }
 )
