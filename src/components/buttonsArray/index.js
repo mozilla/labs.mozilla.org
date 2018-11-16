04fe221962a24ba2b4a550af8cdc245fd3d277ac
Vlad Shumov <shumov.vlad@gmail.com>
2018-11-16 14:56:41 +0300

    bug fix

--- src/components/buttonsArray/index.js
+++ src/components/buttonsArray/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 
+import md from '../../utils/md'
+
 import styles from './index.scss'
 
 export default withStyles(styles)(
@@ -14,9 +16,14 @@ export default withStyles(styles)(
         }
 
         render() {
-            const {buttons} = this.props;
+            const {
+                buttons,
+                description
+            } = this.props;
 
             return <section className={styles.content}>
+                <div dangerouslySetInnerHTML={{__html: md.render(description)}}/>
+
                 <div className={styles.inner}>
                     {buttons && buttons.length && buttons
                         .filter(Boolean)
